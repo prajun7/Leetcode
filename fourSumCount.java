75c896fe49fb504843ed5a0ee56ff68248315596
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-02-03 12:19:59 -0600

    Update fourSumCount.java

--- fourSumCount.java
+++ fourSumCount.java
@@ -9,7 +9,6 @@ Algorithm:
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         HashMap<Integer, Integer> hMap = new HashMap<>();
-        
         int count = 0;
         for (int i = 0; i < nums1.length; i++){
             for (int j = 0; j <nums2.length; j++){
