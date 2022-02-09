3a19b87d0c22700819c2bdcf2fe8997892497bfe
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-02-09 14:57:39 -0600

    Create findPairs.java

---
+++ findPairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        int pairs = 0, n = nums.length;
+        
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> repeatedElements = new HashSet<>();
+        
+        for(int ele : nums){
+            if(set.contains(ele)) repeatedElements.add(ele);
+            set.add(ele);
+        }
+        
+        // SPECIAL CASE [K == 0]
+        if(k == 0) return repeatedElements.size();
+        
+        // Traversing the SET not the array.
+        for(int elementInSet : set)
+            if(set.contains(elementInSet + k)) pairs++;
+       
+        return pairs;
+    }
+}
