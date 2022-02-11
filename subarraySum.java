27b0be547d95f58bd27b5ae69b67db9a212bd6be
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-02-10 22:53:29 -0600

    Create subarraySum.java

---
+++ subarraySum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        
+        HashMap<Integer,Integer> hMap = new HashMap<>();
+        
+        int count = 0;
+        int prefixSum = 0;
+        for (int i = 0; i < nums.length ; i++){
+            prefixSum += nums[i];
+            int diff = prefixSum - k;
+            
+            if (prefixSum == k) count++;
+            
+            if (hMap.containsKey(diff)){
+                count+= hMap.get(diff);
+            }
+            hMap.put(prefixSum, hMap.getOrDefault(prefixSum,0) + 1);
+            
+        }
+        return count;
+        
+    }
+}
