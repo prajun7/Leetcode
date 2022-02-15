5339dda76753fe1de26a012283d13a3e24a1328a
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-02-15 12:21:54 -0600

    Time: 1 ms (95.92%), Space: 50.7 MB (27.62%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,9 +1,14 @@
 class Solution {
+    // Using XOR
+    // a XOR a is equal to 0, that is when we XOR same number we get 0
+    // 4 xor 1 xor 2 xor 1 xor 2 , here 1 and 2 will cancel out and we will be left with 4, which is our answer
+    
     public int singleNumber(int[] nums) {
         int res = nums[0];
         for (int i = 1; i < nums.length ; i++){
             res = res ^ nums[i];
         }
+        
         return res;
         
     }
