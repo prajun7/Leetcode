dc9f9ea4d3c323e48ba460464929121bad7d1a7a
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-02-26 00:18:57 -0600

    Time: 32 ms (20.76%), Space: 64.4 MB (8.17%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -4,7 +4,7 @@ class Solution {
         Map<String, List<String>> map = new HashMap<>();
         for(String w : strs){
             String key = hash(w);          
-            if(!map.containsKey(key)) map.put(key, new LinkedList<>());
+            if(!map.containsKey(key)) map.put(key, new ArrayList<>());
             map.get(key).add(w);
         }
         
@@ -15,5 +15,8 @@ class Solution {
         int[] a = new int[26];
         for(char c : s.toCharArray()) a[c-'a']++;
         return Arrays.toString(a);
+        // Arrays.toString will give string in this format, 
+        // delimited by commas 
+        // --> [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
 }
