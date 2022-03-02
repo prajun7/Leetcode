2bdbb337c444261133145ee59ca3685c927c457b
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-03-02 14:47:43 -0600

    Time: 1 ms (88.48%), Space: 41.8 MB (41.34%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,7 +1,14 @@
+/*
+The idea is to loop through the longer string that is t,
+and compare the character with s,
+If they are same increment the Index of String s
+At any point if indexOf S is equal to the length of the string s then it return true
+*/
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
-        if (s.length() == 0){
+        if (s.length() == 0){ 
             return true;
         }
         
