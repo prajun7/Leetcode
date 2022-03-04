7bb81285422e02dd393f9435312548ef8a089bde
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-03-04 00:39:26 -0600

    Time: 0 ms (100.00%), Space: 41.7 MB (43.76%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -3,15 +3,25 @@ class Solution {
 
         int n = digits.length;
         for(int i=n-1; i>=0; i--) {
-            if(digits[i] < 9) {
+            
+            // This case is for all the number less than 9,
+            // Anytime we see a number less than 9, while iterating from the back
+            // We can just increment that number and return it
+            // This will work for 899 or 12321 or 21399 or any number other than all 9
+            
+            if(digits[i] < 9) {             
                 digits[i]++;
                 return digits;
             }
             digits[i] = 0;
         }
 
-        int[] newNumber = new int [n+1];
-        newNumber[0] = 1;
-        return newNumber;
+        // This case is for when the number is 99999 only or 99 or 9999999
+        // We just create a array of that size and add 1 at the beginning
+        // Rest, numbers in array will be 0, automatically
+         int[] newNumber = new int [n+1];
+         newNumber[0] = 1;
+         return newNumber;
+        
     }
 }
