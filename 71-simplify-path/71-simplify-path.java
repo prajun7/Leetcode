0a826da582bd300df49c5bdf12c92c2637764ccd
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-03-14 17:29:02 -0500

    Time: 8 ms (52.62%), Space: 42.7 MB (63.94%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -1,37 +1,34 @@
 // https://www.youtube.com/watch?v=B6THIJXrTLs
 
 class Solution {
-    
     // TC : O(n)
     // SC : O(n)
-    
     public String simplifyPath(String path) {
         Stack<String> st = new Stack<>();
-        String[] pathList = path.split("/");  // split by backslash, using escape character
+        String[] pathList = path.split("/");  // split by backslash character
 
         for(String p : pathList){
-            if(p.length() == 0 || p.equals(".")){
+            if(p.length() == 0 || p.equals(".")){  // if p is empty just continue
                 continue;
-            } else if(p.equals("..")){
+            } else if(p.equals("..")){   // if two dots, pop from the stack
                 if(!st.empty()){
                     st.pop();
                 }
-            } else {
+            } else {      
                 // string case
                 st.push(p);
             }
         }
+        
         // StringBuilder
         String ans = "";
         while(!st.empty()){
             ans =st.pop() +"/"+ ans;
         }
-
         if(ans.length() == 0){
             return "/";
-        }else{
+        }else{                      // add the end we need to add backslash at the end
             return "/" + ans.substring(0, ans.length() -1);
         }
-
     }
 }
