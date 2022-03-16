b9a98f732281810061255fa4dfa21522ae1b62bc
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-03-16 18:46:49 -0500

    Time: 3 ms (83.65%), Space: 41.9 MB (87.75%) - LeetHub

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st = new Stack<>();
+        
+        int j = 0;
+        for (int i = 0; i < pushed.length ; i++){
+            st.push(pushed[i]);
+            while (!st.isEmpty() && st.peek() == popped[j]){
+               // System.out.println(st.peek());
+                st.pop();
+                j++;
+            }
+
+        }
+        
+        return j == popped.length;
+    
+
+    }
+}
+
+// class Solution {
+//     public boolean validateStackSequences(int[] pushed, int[] popped) {
+//         Stack<Integer> st = new Stack<>();
+//         int j = 0;
+//         for(int p : pushed) {
+//             st.push(p);
+//             while(!st.isEmpty() && j < popped.length && st.peek() == popped[j]) {
+//                 st.pop();
+//                 j++;
+//             }
+//         }
+//         return j == popped.length;
+//     }
+// }
