d4ec1653642a2af164afd4081ebf6ce87094c4a9
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-03-16 18:55:46 -0500

    Time: 6 ms (21.57%), Space: 45.8 MB (13.48%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,3 +1,14 @@
+// https://www.youtube.com/watch?v=SeTsK_aNUWI
+
+/*
+The idea is to push the element of pushed array inside tha stack.
+And after pushing we need to check each time, if theh peek of the stack is
+equal to the element of the popped array. If it is equal we just pop that number from the stack.
+if the j pointer reaches till the end of the array, that means we return true.
+Another way to check if, the stack is empty or not.
+If the stack is empty we return true else false.
+*/
+
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> st = new Stack<>();
@@ -6,30 +17,14 @@ class Solution {
         for (int i = 0; i < pushed.length ; i++){
             st.push(pushed[i]);
             while (!st.isEmpty() && j < popped.length && st.peek() == popped[j]){
-               // System.out.println(st.peek());
                 st.pop();
                 j++;
             }
-
         }
         
-        return j == popped.length;
+       // return j == popped.length;
+        return st.isEmpty();
     
 
     }
 }
-
-// class Solution {
-//     public boolean validateStackSequences(int[] pushed, int[] popped) {
-//         Stack<Integer> st = new Stack<>();
-//         int j = 0;
-//         for(int p : pushed) {
-//             st.push(p);
-//             while(!st.isEmpty() && j < popped.length && st.peek() == popped[j]) {
-//                 st.pop();
-//                 j++;
-//             }
-//         }
-//         return j == popped.length;
-//     }
-// }
