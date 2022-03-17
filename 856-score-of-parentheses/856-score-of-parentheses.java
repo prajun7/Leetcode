00da7a70203ff9a5762d0088064e9eba7b1f3a30
Prajun Trital <67757251+prajun77@users.noreply.github.com>
2022-03-17 11:37:54 -0500

    Time: 1 ms (68.54%), Space: 41.9 MB (40.09%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,3 +1,32 @@
+// https://www.youtube.com/watch?v=jfmJusJ0qKM
+
+/*
+The idea is to whenever we see opening parentheses, just push the score into the stack.
+After pushing that score, make the score again to 0 for the next pari
+If we see a closing parentheses, just pop the score and do the max of (score *2 or 1)
+
+Dry run for,
+((()()))
+
+1. for (
+Stack [0] , score = 0
+
+2. for (
+Stack [0,0] , score = 0
+
+3. for (
+Stack [0,0,0] , score = 0
+
+4. for )
+Stack [0,0] , score = 0 + Math.max(0*2, 1) = 1, so score = 1
+
+5. for (
+Stack [0,0,1] , score = 0
+
+.... Watch the above video
+
+*/
+
 class Solution {
     public int scoreOfParentheses(String s) {
         Stack<Integer> st = new Stack<>();
