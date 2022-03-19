174fa6bf03a78766b78f76786cc819fbcfddbc76
Prajun Trital <67757251+prajun7@users.noreply.github.com>
2022-03-19 11:16:15 -0500

    Time: 52 ms (53.01%), Space: 72.5 MB (31.49%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.java
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -1,3 +1,19 @@
+// https://www.youtube.com/watch?v=Jv5_J1fSju0
+
+/* Algorithm
+1. Create HashMap to keep track of the frequency of the element
+that is, <element, frequency>
+
+2. List of Stack. The index of the List will act as the frequency of the list
+If index of the List is 0, then all the element present at this level will have frequency 1
+If index of the List is 1, then all the element present at this level will have frequency 2
+If index of the List is 2, then all the element present at this level will have frequency 3
+... and so on,
+SO, we need to subtract 1 from the frequency
+The list will store Stack in each of its index
+Watch the above video for more explanation
+*/
+
 class FreqStack {
 
     // SC : O(n)
@@ -7,13 +23,12 @@ class FreqStack {
     Map<Integer, Integer> map = null;
 
     public FreqStack() {
-        map = new HashMap<>();
+        map = new HashMap<>();          // Initiate the map and List
         stList = new ArrayList<>();
-
     }
 
     public void push(int x) {
-        map.put(x, map.getOrDefault(x, 0)+1);
+        map.put(x, map.getOrDefault(x, 0)+1);   // Keep track of the frequency in the HashMap
         int freq = map.get(x);
         if(freq -1 >= stList.size()){
             stList.add(new Stack<Integer>());
