6c0d6c573743f130d89f15b6241f89b4d4c7fb51
Prajun Trital <67757251+prajun7@users.noreply.github.com>
2022-03-21 15:13:11 -0500

    Time: 2 ms (60.42%), Space: 50 MB (41.46%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,4 +1,12 @@
-// We will use binary search
+// We will use modified binary search
+/*
+Algorithm:
+1. Add the end and start element and name it sum
+2. If sum is less than target, move the start pointer to get bigger value
+3. If sum is more than target, move the end pointer to get smaller value
+4. If sum == target, break
+5. Add 1 to both start and end, see question
+*/
 
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
@@ -7,7 +15,6 @@ class Solution {
         
         int start = 0;
         int end = numbers.length - 1;
-        
         
         while (true){
             int sum = numbers[start] + numbers[end];
