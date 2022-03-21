0edda4de94818905819200db22ffa31ed8e4b455
Prajun Trital <67757251+prajun7@users.noreply.github.com>
2022-03-21 14:30:34 -0500

    Time: 1 ms (100.00%), Space: 55.1 MB (19.02%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,3 +1,9 @@
+/*
+See Hints in leetcode,
+1. In-place means we should not be allocating any space for extra array. But we are allowed to modify the existing array. However, as a first step, try coming up with a solution that makes use of additional space. For this problem as well, first apply the idea discussed using an additional array and the in-place solution will pop up eventually.
+
+*/
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int j = 0;
@@ -7,8 +13,8 @@ class Solution {
                 j++;
             }
         }
-        //j = 3
         
+        // Calculate number of times to loop
         int loop = nums.length - j;
         
         int lastIndex = nums.length - 1;
