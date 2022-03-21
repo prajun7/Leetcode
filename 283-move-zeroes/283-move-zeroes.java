73f8461343c90e2cd9e7ddf36df6845fd0aa36ee
Prajun Trital <67757251+prajun7@users.noreply.github.com>
2022-03-21 14:36:31 -0500

    Time: 1 ms (100.00%), Space: 43.6 MB (87.42%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -2,6 +2,10 @@
 See Hints in leetcode,
 1. In-place means we should not be allocating any space for extra array. But we are allowed to modify the existing array. However, as a first step, try coming up with a solution that makes use of additional space. For this problem as well, first apply the idea discussed using an additional array and the in-place solution will pop up eventually.
 
+Algorithm,
+1. Whenever you encounter non zero number just add that number to the beginning of the array
+2. After that at the end of the array just add zeroes
+
 */
 
 class Solution {
@@ -14,11 +18,12 @@ class Solution {
             }
         }
         
+        // j represents the number of non zero number also
         // Calculate number of times to loop
         int loop = nums.length - j;
         
+        // Add zeroes from the end
         int lastIndex = nums.length - 1;
-        
         for (int i = 0; i < loop  ; i++){
             nums[lastIndex] = 0;
             lastIndex--;
