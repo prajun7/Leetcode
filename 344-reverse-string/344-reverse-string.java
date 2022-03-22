6b3ce9d84f44accb1ae114eb3c4c127e04673f6a
Prajun Trital <67757251+prajun7@users.noreply.github.com>
2022-03-22 09:49:00 -0500

    Time: 1 ms (86.33%), Space: 54.1 MB (66.02%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    //Replace start and end charatcters until start < end
+    
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+        while(start <= end){
+            char tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            start++;
+            end--;
+            
+        }
+        
+    }
+}
