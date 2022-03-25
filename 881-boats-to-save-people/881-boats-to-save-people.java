046b50c806027450c15bb1415125ad7e226521f0
Prajun Trital <67757251+prajun7@users.noreply.github.com>
2022-03-24 20:11:20 -0500

    Time: 14 ms (97.15%), Space: 50.4 MB (89.70%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boatCount = 0;
+    
+        Arrays.sort(people);
+        // After sorting we will have light weigth person on the left
+        // and heavy weigth person on the right
+        
+        int left = 0;  
+        int right = people.length - 1;
+        
+        while(left <= right){
+            int sum = people[left] + people[right];
+            
+            // If sum is less than or equal to limit 
+            // we can take both the left(light person) and right(heavy person) 
+            if(sum <= limit){
+                boatCount++;
+                left++;
+                right--;
+            }
+            else{
+                boatCount++;
+                right--;
+            }
+        }
+        return boatCount;
+    }
+}
+
