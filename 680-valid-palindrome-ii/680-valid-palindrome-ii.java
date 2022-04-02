61f74fbf5040f685840a6e032a4f35048250672f
Prajun Trital <67757251+prajun7@users.noreply.github.com>
2022-04-02 10:00:58 -0400

    Time: 10 ms (59.94%), Space: 54.6 MB (48.65%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,34 @@
+/*
+Follow up question was asked to explain how I will extend this to work for n changes. 
+My answer was to have a sub-function that compares left and right and calls recursively with a counter.
+*/
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+    
+    /* Check is s[i...j] is palindrome. */
+    private boolean isPalindrome(String s, int i, int j) {
+        
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+}
